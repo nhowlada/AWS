82b23a63d3c2911ff89f738dea9072e2510a37a7
nhowlada <110633171+nhowlada@users.noreply.github.com>
2023-03-10 09:04:50 -0600

    Update unload.py

--- redshift_s3/redshiftS3/databases/redshift/unload.py
+++ redshift_s3/redshiftS3/databases/redshift/unload.py
@@ -3,6 +3,6 @@ import datetime
 
 unload = f"""unload ('SELECT * FROM cards_ingest.tran_fact_stage')
 to 's3://nhowlada-unload-redshift-10000002/users_{str(datetime.datetime.now())}.csv'
-CREDENTIALS 'aws_access_key_id=AKIAY3QAEFK4LQGEN62M;aws_secret_access_key=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'
+CREDENTIALS 'aws_access_key_id=;aws_secret_access_key='
 parallel off
 csv;"""
